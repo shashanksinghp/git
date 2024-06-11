492db128c25fadbda01271a5a9bc257f8bc4f92f
shashank <shashank>
2024-06-11 11:15:59 +0530

    m

--- python/exception.py
+++ python/exception.py
@@ -72,5 +72,58 @@ def division(a, b):
     except ZeroDivisionError as ex:
         raise ValueError("b must not zero")
 
+#raise from
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as ex:
+        raise ValueError('b must not be zero') from ex
 
-division(1, 0)
+divide(10, 0)
+
+def divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError as ex:
+        raise ValueError('b must not be zero') from ex
+
+
+try:
+    divide(10, 0)
+except ValueError as ex:
+    print('cause:', ex.__cause__)
+    print('exception:', ex)
+
+#custom
+class FahrenheitError(Exception):
+    min_f = 32
+    max_f = 212
+
+    def __init__(self, f, *args):
+        super().__init__(args)
+        self.f = f
+
+    def __str__(self):
+        return f'The {self.f} is not in a valid range {self.min_f, self.max_f}'
+
+
+def fahrenheit_to_celsius(f: float) -> float:
+    if f < FahrenheitError.min_f or f > FahrenheitError.max_f:
+        raise FahrenheitError(f)
+
+    return (f - 32) * 5 / 9
+
+
+if __name__ == '__main__':
+    f = input('Enter a temperature in Fahrenheit:')
+    try:
+        f = float(f)
+    except ValueError as ex:
+        print(ex)
+    else:
+        try:
+            c = fahrenheit_to_celsius(float(f))
+        except FahrenheitError as ex:
+            print(ex)
+        else:
+            print(f'{f} Fahrenheit = {c:.4f} Celsius')
