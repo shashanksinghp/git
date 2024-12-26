5c1d203207369f85151e90010a6c1012b2403be7
shashank <shashank>
2024-12-26 16:17:52 +0530

    changes in file

--- python/clouser.py
+++ python/clouser.py
@@ -11,3 +11,12 @@ def greet(name):
 # call outer function
 greet("John")
 
+#demo 
+def greet(name):
+    # inner function
+    def display_name():
+        print("Hi", name)
+
+    # call inner function
+    display_name()
+
